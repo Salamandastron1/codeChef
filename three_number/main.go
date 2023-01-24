b8d5017fb209b15d9022464d21a6aad74987ad15
Tim Garrity <tag.garrity@gmail.com>
2023-01-24 17:38:36 -0500

    comment explanation

--- three_number/main.go
+++ three_number/main.go
@@ -13,6 +13,10 @@ func main() {
 	scanner.Scan()
 
 	for scanner.Scan() {
+		// this is necessary to save memory
+		// if the compiler thinks it will need
+		// additional memory then the underlying array
+		// will be doubled giving us capacity we don't need
 		xi := make([]int, 3, 3)
 		temps := strings.Split(scanner.Text(), " ")
 		for i, v := range temps {
