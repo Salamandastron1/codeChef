7b1683d8fa5489b1e8a0aa7927bb3eeb3fc4de29
Tim Garrity <tag.garrity@gmail.com>
2023-01-27 18:24:58 -0500

    make sure that each digit is unique aka pairwise different

---
+++ find_int/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var x int
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	for scanner.Scan() {
+		x = toInt(scanner.Bytes())
+		num := x + 1
+		for !unique(num) {
+			num++
+		}
+		fmt.Println(num)
+	}
+}
+func unique(i int) bool {
+	memo := map[int]bool{}
+
+	for i > 0 {
+		digit := i % 10
+		if ok := memo[digit]; ok {
+			return false
+		}
+
+		memo[digit] = true
+		i /= 10
+	}
+	return true
+}
+
+func toInt(buf []byte) (n int) {
+	for _, v := range buf {
+		n = n*10 + int(v-'0')
+	}
+	return
+}
